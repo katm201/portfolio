640adea2389ba135569094bf522c420ca35a6ae4
Katherine Manning <katm201@gmail.com>
2018-04-01 15:19:52 -0700

    [fix] remove NotFound from Routes (double render)

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -8,11 +8,10 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './components/Home';
-import NotFound from './components/NotFound';
+// import NotFound from './components/NotFound';
 
 export default () => (
   <div>
     <Route exact path="/" component={Home} />
-    <Route component={NotFound} />
   </div>
 );
