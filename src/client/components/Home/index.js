adc332c4b2876a74d10f8a033d85c278d4d286dc
Katherine Manning <katm201@gmail.com>
2018-04-01 14:58:28 -0700

    [feat] add copy & styling to body of home page

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -15,7 +15,18 @@ const Home = () => (
     <Header />
     <section className="e-home-img"></section>
     <section className="e-home-main">
-      {'Home'}
+      <h1 className="m-header">
+        {'Welcome'}
+      </h1>
+      <p className="m-paragraph">
+        {'I\'m a full stack software engineer based in the SF Bay Area specializing in JavaScript frameworks for both frontend and backend development. I\'m at my best when diving into a new problem--building new features, learning new technologies, or tackling hard to solve bugs. I\'m passionate about creating well written, well thoughout, and well organized code that improves user and developer experience alike.'}
+      </p>
+      <p className="m-paragraph">
+        {'Visit my Skills page to view a current list of technologies with which I have experience or visit my Projects page to see coding examples and walkthroughs from personal projects.'}
+      </p>
+      <span className="m-small">
+        {`${decodeURI('%C2%A9')} Katherine Manning 2018`}
+      </span>
     </section>
     <BottomBar />
   </section>
